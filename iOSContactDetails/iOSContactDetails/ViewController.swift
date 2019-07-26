86ae3d41ee8a10cb3fa3776bca2120945f0d2678
Balraj Singh <erbalrajs@gmail.com>
2019-07-26 16:43:15 +0530

    Created workspace and added iOS project

---
+++ iOSContactDetails/iOSContactDetails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSContactDetails
+//
+//  Created by Balraj Singh on 26/07/19.
+//  Copyright © 2019 Balraj Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
