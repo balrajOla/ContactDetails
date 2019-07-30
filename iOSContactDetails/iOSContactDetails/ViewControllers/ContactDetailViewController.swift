1a732a6c1c3fe2eac5f78bf19371532b771d00bc
Balraj Singh <erbalrajs@gmail.com>
2019-07-30 13:08:26 +0530

    fixed gradient issue on orientation change

--- iOSContactDetails/iOSContactDetails/ViewControllers/ContactDetailViewController.swift
+++ iOSContactDetails/iOSContactDetails/ViewControllers/ContactDetailViewController.swift
@@ -14,6 +14,7 @@ class ContactDetailViewController: UIViewController {
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var emailID: UILabel!
     @IBOutlet weak var mobileNumber: UILabel!
+    @IBOutlet weak var grdientView: GradientView!
     
     let viewModel: ContactDetailsViewModel
     
@@ -24,6 +25,12 @@ class ContactDetailViewController: UIViewController {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        grdientView.setNeedsDisplay()
     }
     
     override func viewDidLoad() {
